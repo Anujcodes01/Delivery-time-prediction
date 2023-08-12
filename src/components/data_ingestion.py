44644b5075fbabcf2482abe9be2bd6302adcfdde
Anujdh8755 <anujdhyani232@gmail.com>
2023-08-12 18:05:07 +0530

    data ingestion ,data transformation,model training completed.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -50,18 +50,18 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 #Data Ingestion
-# if __name__ == "__main__":
-#     obj = DataIngestion()
-#     train_data,test_data = obj.initiate_data_ingestion()
-#     data_transformation = DataTransformation()
-#     train_arr, test_arr , processed_obj_file_path= data_transformation.inititate_data_transformation(train_data, test_data)
-#
-# # Data Transformation
-# if __name__ == "__main__":
-#     obj = DataIngestion()
-#     train_data_path,test_data_path = obj.initiate_data_ingestion()
-#     data_transformation = DataTransformation()
-#     train_arr,test_arr = data_transformation.inititate_data_transformation(train_data_path,test_data_path)
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr , processed_obj_file_path= data_transformation.inititate_data_transformation(train_data, test_data)
+
+# Data Transformation
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data_path,test_data_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr ,_= data_transformation.inititate_data_transformation(train_data_path,test_data_path)
 
 
 #model trainer
